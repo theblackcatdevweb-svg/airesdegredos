efa58ef9a9359ae61d3ba0362e9d0a89379065b1
The Black Cat Dev Web <theblackcatdevweb@gmail.com>
2026-01-05 01:45:17 +0100

    Configuración de Netlify, Clerk y Stripe completada

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import clerk from '@clerk/astro';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [
     tailwind(), 
     clerk()
   ],
-  output: 'server'
+
+  output: 'server',
+  adapter: netlify()
 });
